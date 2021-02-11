4c70736b295fe3fcd53794f6c388567f2d872175
RTurney <r.turney93@gmail.com>
2021-02-11 15:01:28 +0000

    add .up spec. One test red

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -12,4 +12,12 @@ describe('Thermostat', () => {
       expect(thermostat.temperature()).toEqual(20);
     });
   });
+
+  describe('.up', () => {
+
+    it('increases the temperature of the thermostat', () => {
+      thermostat.up();
+      expect(thermostat.temperature()).toEqual(21);
+    });
+  })
 });
